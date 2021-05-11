1286b5749dd87c2682cdbe5bf464e82f28db2109
bloup-dv <naujox.tom@gmail.com>
2021-05-11 23:43:51 +0200

    Initial commit

---
+++ src/main/java/com/example/springdatajpapresentation/SpringdatajpaPresentationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpapresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdatajpapresentation/SpringdatajpaPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpapresentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaPresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
